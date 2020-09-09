21716fcc6fcb7c5411a69c4f37589b07dfb4bc35
faizanshar <muhammadfaizanshar@gmai.com>
2020-09-09 20:04:40 +0700

    first commit

---
+++ TUGAS16(1).js
@@ -0,0 +1,9 @@
+var panjang = 8;
+var lebar = 6;
+
+function  total(panjang, lebar){
+    var total = panjang * lebar;
+    return total;
+
+}
+document.write("Jadi hasil dari luas persegi panjang adalah = " + total(panjang,lebar))
